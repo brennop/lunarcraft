49be3ef7fc6d05a764ad1ef3b80ca300d8a12bf8
brennop <brennopcordeiro@gmail.com>
2023-03-20 11:22:25 -0300

    wip: torch lighting

--- chunk.lua
+++ chunk.lua
@@ -19,13 +19,17 @@ function Chunk:new(x, y, z, world)
   self.world = world
 
   self.blocks = {}
+  self.lightMap = {}
+  self.toUpdate = {}
 
   self.loaded = false
 
   for i = 1, CHUNK_SIZE do
     self.blocks[i] = {}
+    self.lightMap[i] = {}
     for j = 1, CHUNK_HEIGHT do
       self.blocks[i][j] = {}
+      self.lightMap[i][j] = {}
       for k = 1, CHUNK_SIZE do
         local x, y, z = self.position.x + i, self.position.y + j, self.position.z + k
 
@@ -43,6 +47,8 @@ function Chunk:new(x, y, z, world)
         else
           self.blocks[i][j][k] = 0
         end
+
+        self.lightMap[i][j][k] = 0
       end
     end
   end
@@ -57,6 +63,18 @@ function Chunk:new(x, y, z, world)
 
   self.start = 1
   self.step = 4
+end
+
+local function encodeIndex(i, j, k)
+  return i + (j-1) * CHUNK_SIZE + (k-1) * CHUNK_SIZE * CHUNK_HEIGHT
+end
+
+local function decodeIndex(index)
+  local i = (index - 1) % CHUNK_SIZE + 1
+  local j = math.floor((index - 1) / CHUNK_SIZE) % CHUNK_HEIGHT + 1
+  local k = math.floor((index - 1) / (CHUNK_SIZE * CHUNK_HEIGHT)) % CHUNK_SIZE + 1
+
+  return i, j, k
 end
 
 function Chunk:load()
@@ -75,6 +93,19 @@ function Chunk:load()
   end
 
   self.thread:start(self.position:table(), blocks, self.channel, blockTypes)
+end
+
+function Chunk:updateLight()
+  for i = 1, CHUNK_SIZE do
+    for j = 1, CHUNK_HEIGHT do
+      for k = 1, CHUNK_SIZE do
+        local block = self.blocks[i][j][k]
+        if block <= 0 then
+          self.blocks[i][j][k] = self.lightMap[i][j][k] - 16
+        end
+      end
+    end
+  end
 end
 
 function Chunk:setFace(index, mesh, x, y, z, value)
@@ -153,8 +184,72 @@ function Chunk:setBlock(x, y, z, block)
 
   if self.blocks[i][j][k] == block then return end
 
+  if block > 0 then self:setTorch(i, j, k) end
+
   self.blocks[i][j][k] = block
-  self.world:updateBlockMesh(x, y, z)
+  -- self.world:updateBlockMesh(x, y, z)
+end
+
+local normals = {
+  {  0,  0,  1 },
+  {  0,  1,  0 },
+  {  0,  0, -1 },
+  {  0, -1,  0 },
+  {  1,  0,  0 },
+  { -1,  0,  0 },
+}
+
+function Chunk:setTorch(i, j, k)
+  self.lightMap[i][j][k] = 15
+
+  local index = encodeIndex(i, j, k)
+  local queue = { index }
+
+  while #queue > 0 do
+    local index = table.remove(queue, 1)
+    local i, j, k = decodeIndex(index)
+
+    if i > 0 and i <= CHUNK_SIZE and j > 0 and j <= CHUNK_HEIGHT and k > 0 and k <= CHUNK_SIZE then
+      local light = self.lightMap[i][j][k]
+
+      for n = 1, 6 do
+        local dir = normals[n]
+        local ni, nj, nk = i + dir[1], j + dir[2], k + dir[3]
+        -- local x, y, z = ni + self.position.x, nj + self.position.y, nk + self.position.z
+
+        -- local chunk = self.world:getChunk(x, z)
+        -- local block = chunk:getBlock(x, y, z)
+
+        -- local ci, cj, cz = x - chunk.position.x, y - chunk.position.y, z - chunk.position.z
+        -- if block <= 0 and chunk.lightMap[ci][cj][cz] + 2 <= light then
+        --   chunk.lightMap[ci][cj][cz] = light - 1
+        --   local newIndex = i * CHUNK_SIZE * CHUNK_HEIGHT + j * CHUNK_SIZE + k
+        -- end
+
+        if ni > 0 and ni <= CHUNK_SIZE and nj > 0 and nj <= CHUNK_HEIGHT and nk > 0 and nk <= CHUNK_SIZE then
+          local block = self.blocks[ni][nj][nk]
+          if block <= 0 and self.lightMap[ni][nj][nk] + 2 <= light then
+            self.lightMap[ni][nj][nk] = light - 1
+
+            self.toUpdate[index] = true
+            for n2 = 1, 6 do
+              local dir2 = normals[n2]
+              local ni2, nj2, nk2 = ni + dir2[1], nj + dir2[2], nk + dir2[3]
+              local index2 = encodeIndex(ni2, nj2, nk2)
+              if ni2 > 0 and ni2 <= CHUNK_SIZE and nj2 > 0 and nj2 <= CHUNK_HEIGHT and nk2 > 0 and nk2 <= CHUNK_SIZE then
+                self.toUpdate[index2] = { ni2, nj2, nk2 }
+              end
+            end
+
+            local newIndex = encodeIndex(ni, nj, nk)
+            table.insert(queue, newIndex)
+          end
+        end
+      end
+    end
+  end
+
+  self:updateLight()
 end
 
 function Chunk:update()
@@ -164,6 +259,13 @@ function Chunk:update()
     local vertices, start, count = unpack(message)
     self.mesh:setVertices(vertices, start, count)
   end
+
+  for index, v in pairs(self.toUpdate) do
+    local i, j, k = decodeIndex(index)
+    self:updateBlockMesh(i + self.position.x, j + self.position.y, k + self.position.z)
+  end
+
+  self.toUpdate = {}
 end
 
 function Chunk:draw()
